1c5239d3f621f6d23952cda97512ef2e9d8ea56c
Owojori Micheal <owojorimykel@yahoo.com>
2022-06-12 20:13:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let ban = document.getElementById('ban')
+// let bannerEle= document.getElementsByClassName('banner')[0];
+let bookBoxEle = document.getElementsByClassName('book-box');
+let bestsellersContainerEle = document.getElementsByClassName('bestsellers-container')[0];
+
+ban.addEventListener("click",()=>{
+    bestsellersContainerEle.style.display='block';
+    bookBoxEle.style.justifySelf='end';
+})
+console.log(ban);
+console.log(bestsellersContainerEle);
+console.log(bookBoxEle);
+
+// bannerEle.addEventListener("click", function () {
+// })
+
+let age=10;
+
+bookBoxEle.style.justifySelf= "end";
+console.log(age);
+
+// let = buttonRoll = document.getElementById("myButton")
+// buttonRoll.addEventListener("click", function(){
+//     console.log("Button Clicked")
+// })
+
+// let = bannerEle = document.get
+
